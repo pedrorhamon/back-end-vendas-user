cdb9294372ccceb88e11060035fc1f5b1da84644
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-04-09 19:02:49 -0300

    created project]

---
+++ src/main/java/com/starking/user_vendas/UserVendasApplication.java
@@ -0,0 +1,13 @@
+package com.starking.user_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserVendasApplication.class, args);
+	}
+
+}
