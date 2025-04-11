ea1944ffd170883eebe3d46b74d73fcb271578d2
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-04-11 19:53:30 -0300

    Adicionando class para autenticação e jwt

---
+++ src/main/java/com/starking/user_vendas/config/SecurityConfig.java
@@ -0,0 +1,83 @@
+package com.starking.user_vendas.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final SecurityUserDetailsService userDetailsService;
+
+    private final JwtService jwtService;
+
+
+    private static final String[] AUTH = {  "/api/pessoas/**",
+            "/api/categorias/**", "/api/lancamentos/**",
+            "/api/usuarios/**", "/api/permissoes/**", "/api/sub-permissoes/**", "/v3/api-docs/**",
+            "/swagger-ui/**", "/swagger-ui.html","/swagger-resources/**", "/webjars/**"};
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
+
+    @Bean
+    public JwtTokenFilter jwtTokenFilter() {
+        return new JwtTokenFilter(jwtService, userDetailsService);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(AUTH).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Apenas uma origem
+//        config.setAllowedOrigins(Arrays.asList("http://localhost")); // Melhorar isso aqui
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean(CorsFilter corsFilter) {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>(corsFilter);
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registrationBean;
+    }
+}
---
+++ src/main/java/com/starking/user_vendas/services/JwtService.java
@@ -0,0 +1,122 @@
+package com.starking.user_vendas.services;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Service;
+
+import com.starking.vendas.config.component.JwtProperties;
+import com.starking.vendas.model.response.UsuarioResponse;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import lombok.AllArgsConstructor;
+
+/**
+ * @author pedroRhamon
+ */
+@Service
+@AllArgsConstructor
+public class JwtService {
+
+	private final JwtProperties jwtProperties;
+	private final Set<String> revokedTokens = ConcurrentHashMap.newKeySet();
+	
+	public String gerarToken(UsuarioResponse usuarioResponse) {
+		long exp = Long.valueOf(jwtProperties.getExpiracao());
+		LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(exp);
+		Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
+		java.util.Date data = Date.from(instant);
+
+		String horaExpiracaoToken = dataHoraExpiracao.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"));
+		
+		 List<String> roles = usuarioResponse.getPermissoes();
+
+		String token = Jwts.builder()
+				.setExpiration(data).setSubject(usuarioResponse.getEmail())
+				.claim("userid", usuarioResponse.getId()).claim("name", usuarioResponse.getName())
+				.claim("roles", roles)
+				.claim("horaExpiracao", horaExpiracaoToken).signWith(io.jsonwebtoken.SignatureAlgorithm.HS256, jwtProperties.getChaveAssinatura())
+				.compact();
+
+		return token;
+	}
+
+	public Claims obterClaims(String token) throws ExpiredJwtException {
+		return Jwts
+				.parser()
+				.setSigningKey(jwtProperties.getChaveAssinatura())
+				.parseClaimsJws(token)
+				.getBody();
+	}
+
+
+	public boolean isTokenValido(String token) {
+	    try {
+			if (revokedTokens.contains(token)) {
+				return false;
+			}
+	        Claims claims = obterClaims(token); 
+	        java.util.Date dataEx = claims.getExpiration();
+	        LocalDateTime dataExpiracao = dataEx.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+	        boolean dataHoraAtualIsAfterDataExpiracao = LocalDateTime.now().isAfter(dataExpiracao);
+	        return !dataHoraAtualIsAfterDataExpiracao;
+	    } catch (ExpiredJwtException e) {
+	        return false;
+	    } catch (Exception e) {
+	        return false;
+	    }
+	}
+
+	public String obterLoginUsuario(String token) {
+		Claims claims = obterClaims(token);
+		return claims.getSubject();
+	}
+	
+	 public String gerarRefreshToken(UsuarioResponse usuario) {
+	        long exp = Long.valueOf( jwtProperties.getRefreshExpiracao());
+	        LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(exp);
+	        Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
+	        Date data = Date.from(instant);
+
+	        List<String> roles = usuario.getPermissoes();
+
+	        String refreshToken = Jwts
+	                .builder()
+	                .setExpiration(data)
+	                .setSubject(usuario.getEmail())
+	                .claim("userid", usuario.getId())
+	                .claim("roles", roles)
+	                .signWith(io.jsonwebtoken.SignatureAlgorithm.HS256, jwtProperties.getChaveAssinatura())
+	                .compact();
+
+	        return refreshToken;
+	 }
+	 
+	 public boolean isRefreshTokenValido(String refreshToken) {
+	        try {
+				if (revokedTokens.contains(refreshToken)) {
+					return false;
+				}
+	            Claims claims = obterClaims(refreshToken);
+	            Date dataEx = claims.getExpiration();
+	            LocalDateTime dataExpiracao = dataEx.toInstant()
+	                    .atZone(ZoneId.systemDefault()).toLocalDateTime();
+	            boolean dataHoraAtualIsAfterDataExpiracao = LocalDateTime.now().isAfter(dataExpiracao);
+	            return !dataHoraAtualIsAfterDataExpiracao;
+	        } catch (ExpiredJwtException e) {
+	            return false;
+	        }
+	 }
+	 
+	public void revokeToken(String token) {
+	   revokedTokens.add(token);
+	}
+}
