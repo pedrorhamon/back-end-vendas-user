c1c66ff4a98a9b1cefdf142aaa6bf19f9b2dfb55
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-04-22 21:28:44 -0300

    Adicionando class SecurityUserDetailsService

---
+++ src/main/java/com/starking/user_vendas/services/SecurityUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.starking.user_vendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.starking.vendas.model.Usuario;
+import com.starking.vendas.repositories.UsuarioRepository;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author pedroRhamon
+ */
+@Service
+@AllArgsConstructor
+public class SecurityUserDetailsService implements UserDetailsService{
+	
+	private final UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Usuario usuarioEncontrado = usuarioRepository
+				.findByEmail(email)
+				.orElseThrow(() -> new UsernameNotFoundException("Email não cadastrado"));
+		
+		 List<GrantedAuthority> authorities = usuarioEncontrado.getPermissoes()
+	                .stream()
+	                .map(permissao -> new SimpleGrantedAuthority("ROLE_" + permissao.getName()))
+	                .collect(Collectors.toList());
+		
+		return User.builder()
+				.username(usuarioEncontrado.getEmail())
+				.password(usuarioEncontrado.getSenha())
+				.authorities(authorities)
+				.build();
+	}
+}
