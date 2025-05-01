5b616f2b7746045afdf6706d4325a2deeed12737
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-05-01 19:12:59 -0300

    Adicionando JwtProperties

---
+++ src/main/java/com/starking/user_vendas/config/component/JwtProperties.java
@@ -0,0 +1,37 @@
+package com.starking.user_vendas.config.component;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+/**
+ * @author pedroRhamon
+ */
+@Component
+@ConfigurationProperties(prefix = "jwt")
+@Getter
+@Setter
+public class JwtProperties {
+    private String expiracao;
+    private String chaveAssinatura;
+    private String refreshExpiracao;
+    
+    public JwtProperties() {
+        this.chaveAssinatura = System.getenv("JWT_CHAVE_ASSINATURA");
+        if (this.chaveAssinatura == null) {
+            generateKey();
+        }
+    }
+    
+    private void generateKey() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] key = new byte[32]; // 256 bits
+        secureRandom.nextBytes(key);
+        this.chaveAssinatura = Base64.getEncoder().encodeToString(key);
+        System.out.println("Chave secreta base64 gerada: " + this.chaveAssinatura);
+    }
+}
