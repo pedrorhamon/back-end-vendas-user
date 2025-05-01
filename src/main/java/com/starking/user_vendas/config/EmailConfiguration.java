df8e5ed18a67c97c436d641d0934ffd5adbab5f7
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-05-01 19:31:00 -0300

    corrigindo imports

---
+++ src/main/java/com/starking/user_vendas/config/EmailConfiguration.java
@@ -0,0 +1,36 @@
+package com.starking.user_vendas.config;
+
+import com.starking.user_vendas.config.component.MailProperties;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@AllArgsConstructor
+public class EmailConfiguration {
+
+    private final MailProperties mailProperties;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(mailProperties.getHost());
+        mailSender.setPort(mailProperties.getPort());
+        mailSender.setUsername(mailProperties.getUsername());
+        mailSender.setPassword(mailProperties.getPassword());
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
